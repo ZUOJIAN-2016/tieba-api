e41a5594a78a4042296a980755449f189d090c64
Yun Huang <1613743554@qq.com>
2017-02-23 11:12:40 +0800

    second commit

--- lib/DB_Options.php
+++ lib/DB_Options.php
@@ -3,27 +3,25 @@
 
 class DB_Options
 {
-private $_dbname,$_dbpassword,$_dbhost,$pdo;
+private $_dbname,$_dbpassword,$_dbhost;
 private $sql=array("from"=>"",
         "where"=>"",
         "order"=>"",
         "limit"=>""
     );
 private $tablename;
- private $column=array();
     private $insert=array(
         "tablename"=>array(),
         "column"=>array()
     );
 public function __construct()
     {
-       $this->_dbname='tieba';
-        $this->_dbpassword='';
-        $this->_dbhost='localhost';
+       $this->_dbname='OlFgSNSuFAwTZJwqhVuU';
+        $this->_dbpassword='5dd3981f872f4477b897cb63f1e43410';
+        $this->_dbhost='sqld.duapp.com';
        try {
-           $this->pdo=new PDO("mysql:host=$this->_dbhost;port:3306;dbname=$this->_dbname", "root", "$this->_dbpassword");
-           $this->pdo->setAttribute(PDO::ATTR_PERSISTENT,true);
-           echo $this->_dbname;
+           $pdo=new PDO("mysql:host=$this->_dbhost;port=4050;dbname=$this->_dbname", "da49132671114ca2ae83f45b1614bf75", "$this->_dbpassword");
+           $pdo->setAttribute(PDO::ATTR_PERSISTENT,true);
        }
        catch(PDOException $e)
        {
@@ -44,9 +42,9 @@ public function where($_where='1=1')
 public function select($_select)
 {
     try {
-
+        $pdo = new PDO("mysql:host=$this->_dbhost;port:4050;dbname=$this->_dbname", "root", "$this->_dbpassword");
         $sql="SELECT" . $_select . ' ' . (implode(' ', $this->sql));
-        $res= $this->pdo->query($sql);
+        $res=$pdo->query($sql);
         if ($res) {
            $result=$res->fetchAll(PDO::FETCH_ASSOC);
             return $result;
@@ -60,23 +58,24 @@ public function select($_select)
     }
 }
 //搜索操作
-public function column()
-{
-   $this->insert["column"]= func_get_args();
-   return $this;
-}
-public function table()
-{
-    $this->insert["tablename"]= func_get_args();
-    return $this;
-}
+    public function column()
+    {
+        $this->insert["column"]= func_get_args();
+        return $this;
+    }
+    public function table()
+    {
+        $this->insert["tablename"]= func_get_args();
+        return $this;
+    }
+
 public function insert()
 {
     $source=func_get_args();
     try {
-
+        $pdo = new PDO("mysql:host=$this->_dbhost;port:4050;dbname=$this->_dbname", "root", "$this->_dbpassword");
         $sql="INSERT INTO ".implode('',$this->insert['tablename'])."(".implode(',',$this->insert['column']).")"." VALUES"." "."(". (implode(',',$source)).")";
-        if ($this->pdo->exec($sql)) {
+        if ($pdo->exec($sql)) {
             return 1;
         } else {
             return 0;
@@ -91,9 +90,9 @@ public function insert()
 public function count()
 {
 try{
-
+    $pdo = new PDO("mysql:host=$this->_dbhost;port:4050;dbname=$this->_dbname", "root", "$this->_dbpassword");
     $sql= $sql="'SELECT COUNT(*)FROM '.$this->tablename";
-    $res= $this->pdo->query($sql);
+    $res=$pdo->query($sql);
     $num=$res->columnCount();
     return $num;
 }
@@ -107,8 +106,9 @@ catch(PDOException $e)
     public function fetch_table()
     {
         try{
+            $pdo = new PDO("mysql:host=$this->_dbhost;port:4050;dbname=$this->_dbname", "root", "$this->_dbpassword");
             $sql="'SELECT COUNT(*)FROM '.$this->tablename";
-            $res= $this->pdo->query($sql);
+            $res=$pdo->query($sql);
             $result=$res->fetchAll(PDO::FETCH_ASSOC);
             return $result;
         }
